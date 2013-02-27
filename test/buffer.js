f0ccab0dcd143946d8e3aa2df1536ff8a8d2f061
Tõnis Tiigi <tonistiigi@gmail.com>
2013-02-27 21:10:07 +0200

    Add support for fill()

--- test/buffer.js
+++ test/buffer.js
@@ -119,3 +119,13 @@ test("concat() a varying number of buffers", function (t) {
     t.equal(flatLongLen.toString(), (new Array(10+1).join('asdf')));
     t.end();
 });
+
+test("fill", function(t) {
+    t.plan(1);
+    var b1 = new Buffer(10);
+    var b2 = new buffer.Buffer(10);
+    b1.fill(2);
+    b2.fill(2);
+    t.equal(b1.toString('hex'), b2.toString('hex'));
+    t.end();
+})
